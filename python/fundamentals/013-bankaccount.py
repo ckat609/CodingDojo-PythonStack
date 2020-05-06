b7aff0988f340aecccd2eff771765b2bef3bc758
Armando Tello <ckat609@yahoo.com>
2020-05-06 11:47:14 -0500

    finished bankaccount

---
+++ python/fundamentals/013-bankaccount.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, amount=0, interest=0.01):
+        self.amount = amount
+        self.interest = interest
+        self.display_user_balance = interest
+
+    def deposit(self, amount):
+        self.amount += amount
+        return self
+
+    def withdraw(self, amount):
+        self.amount -= amount
+        return self
+
+    def display_account_info(self):
+        return self.amount
+
+    def yield_interest(self):
+        self.amount += self.amount*self.interest
+        return self
+
+
+armandoBA = BankAccount(500)
+saurabBA = BankAccount(900)
+
+print(f"{armandoBA.deposit(100).deposit(100).deposit(50).withdraw(75).yield_interest().display_account_info()}")
+print(f"{saurabBA.deposit(150).deposit(100).withdraw(75).withdraw(100).withdraw(50).withdraw(50).yield_interest().display_account_info()}")
