0fe4673a17bba566b5bc38082950b2313fec9987
Armando Tello <ckat609@yahoo.com>
2020-05-07 19:19:50 -0500

    finished html table

---
+++ flask/flask_fundamentals/021-FLASK-html-table/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def landing_page():
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'},
+        {'first_name': 'Armando', 'last_name': 'Tello'},
+        {'first_name': 'Lucas', 'last_name': 'Hediondo'},
+        {'first_name': 'Hermoso', 'last_name': 'Chuchu'}
+    ]
+    return render_template("index.html", user_list=users)
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
