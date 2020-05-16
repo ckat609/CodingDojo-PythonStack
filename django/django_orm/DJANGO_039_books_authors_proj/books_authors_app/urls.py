829d551b8605f9a5fa842cd604a3434e54447656
Armando Tello <ckat609@yahoo.com>
2020-05-16 01:15:08 -0500

    finished books and authors template

--- django/django_orm/DJANGO_039_books_authors_proj/books_authors_app/urls.py
+++ django/django_orm/DJANGO_039_books_authors_proj/books_authors_app/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('book/', views.add_book_form),
+    path('author/', views.add_author_form),
+    path('book/<int:id>', views.view_book),
+    path('author/<int:id>', views.view_author),
+    path('add_book_db/', views.add_book_db),
+    path('add_author_db/', views.add_author_db),
+    path('add_author_to_book/<int:id>', views.add_author_to_book),
+    path('add_book_to_author/<int:id>', views.add_book_to_author)
 ]
--- django/django_orm/DJANGO_039_books_authors_proj/books_authors_app/views.py
+++ django/django_orm/DJANGO_039_books_authors_proj/books_authors_app/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from books_authors_app.models import *
 
 
 def index(request):
-    return HttpResponse("Hello World! This is a test for the Books And Authors Assignment.")
+    return render(request, "index.html")
 
-# Create your views here.
+
+def add_book_form(request):
+    # books = Book.objects.all()
+    context = {
+        'books': Book.objects.all()
+    }
+    print(context)
+    return render(request, "add_book.html", context)
+
+
+def add_author_form(request):
+    # books = Book.objects.all()
+    context = {
+        'authors': Author.objects.all()
+    }
+    print(context)
+    return render(request, "add_author.html", context)
+
+
+def view_book(request, id):
+    context = {
+        'book': Book.objects.get(id=id),
+        'author': Author.objects.exclude(books=Book.objects.get(id=id))
+    }
+    print(context)
+    return render(request, "view_book.html", context)
+
+
+def view_author(request, id):
+    context = {
+        'author': Author.objects.get(id=id),
+        'book': Book.objects.exclude(authors=Author.objects.get(id=id))
+    }
+    print(context)
+    return render(request, "view_author.html", context)
+
+
+def add_book_db(request):
+    # /print("*"*50)
+    if(request.POST['book-title'] != "" and request.POST['book-desc'] != ""):
+        Book.objects.create(title=request.POST['book-title'], desc=request.POST['book-desc'])
+    return redirect("/book/")
+
+
+def add_author_db(request):
+    # /print("*"*50)
+    if(request.POST['author-first'] != "" and request.POST['author-last'] != "" and request.POST['author-notes'] != ""):
+        Author.objects.create(first_name=request.POST['author-first'], last_name=request.POST['author-last'], notes=request.POST['author-notes'])
+    return redirect("/author/")
+
+
+def add_author_to_book(request, id):
+    print("*"*50)
+    print(request.POST['new_author'])
+    Book.objects.get(id=id).authors.add(Author.objects.get(id=request.POST['new_author']))
+    return redirect(f"/book/{id}")
+
+
+def add_book_to_author(request, id):
+    print("*"*50)
+    print(request.POST['new_book'])
+    Author.objects.get(id=id).books.add(Book.objects.get(id=request.POST['new_book']))
+    return redirect(f"/author/{id}")
