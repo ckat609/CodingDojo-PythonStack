75a1aa9866cebfb796457503a0ea2059fdf34d85
Armando Tello <ckat609@yahoo.com>
2020-05-19 12:48:11 -0500

    finished semi restful tv show with validation

--- django/django_full_stack/DJANGO_040_semi_restful_shows/shows_app/models.py
+++ django/django_full_stack/DJANGO_040_semi_restful_shows/shows_app/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class ShowManager(models.Manager):
+    def basicValidator(self, postData):
+        errors = {}
+
+        if(len(postData['title']) == 0):
+            errors['title'] = "Show title must contain at least one character."
+        if(len(postData['description']) > 0 and len(postData['description']) < 10):
+            errors['description'] = "Show description can be empty, but if it's included, it should contain at least ten characters."
+        if(len(postData['image']) == 0):
+            errors['image'] = "Image path must be included."
+        if(postData['release_date'] >= datetime.now().strftime('%Y-%m-%d')):
+            errors['release_date'] = "Date can't be set to today or in the future."
+
+        return errors
 
 
 class Network(models.Model):
@@ -17,4 +34,4 @@ class Show(models.Model):
     network = models.ForeignKey(Network, related_name="shows", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now_add=True)
-
+    objects = ShowManager()
